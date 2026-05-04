2da7d80d534cd3d2b45d5bfec37ca2bd8298c1e2
shehab1developer <exe.developer.osc@gmail.com>
2026-05-04 17:29:08 +0300

    Update auth.controller.js sameSite To none (less security)

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -10,7 +10,7 @@ const setTokenCookie = (res, token) =>
     res.cookie('jwt_token', token, {
         httpOnly: true,
         secure:   process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
+        sameSite: 'none',
         maxAge:   7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
