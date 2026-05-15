60abaeb58e066a7d0750652efdd2e68cde4c1a8d
shehab1developer <exe.developer.osc@gmail.com>
2026-05-15 17:16:41 +0300

    handle the contoller of updateUser in auth router

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -5,6 +5,7 @@ const {
   login,
   logout,
   getMe,
+  updateProfile
 } = require("../controllers/auth.controller");
 
 router.post("/register", register);
@@ -13,5 +14,6 @@ router.post("/logout", logout);
 
 router.use(Auth);
 router.get("/me", getMe);
+router.patch('/profile', updateProfile);
 
 module.exports = router;
